0ad18cb69b6d12eedd7b2a70010fcd99263c4556
lincong <lincong8722@qq.com>
2025-09-22 15:19:27 +0800

    DEBUG:fix __init__

--- neurite/__init__.py
+++ neurite/__init__.py
@@ -1,79 +1,51 @@
 """
-Neurite: A modular deep learning library for medical image processing.
-======================================================================
+---- neurite ----
 
-Neurite is organized into two main parts:
-- Backend-specific modules: Each backend (e.g., PyTorch, TensorFlow) is contained in their own
-  subpackages with functionalities such as layers and models.
-- Core Python utilities: Common functionality implemented in pure Python and NumPy, organized under
-  the 'py' subpackage.
+organization:
 
-This module is the top-level initializer for the Neurite package. It performs the following tasks:
-  1. Validates that the required version of pystrum is installed.
-  2. Imports core utilities from the 'py' subpackage.
-  3. Dynamically loads backend-specific modules based on the configured backend.
+we'll have a parent folder for each backend type (e.g. tf or torch). inside each of them is a
+python module for important constructs (e.g. layers), and a utils folder. Inside the folder are
+naturally structured modules (e.g. seg.py).
 
-Configuration
--------------
-The backend is determined by the environment variable `NEURITE_BACKEND`. The default backend is
-TensorFlow unless `NEURITE_BACKEND` is set to 'pytorch'.
+separately, we'll have a python utilities folder (py), which contains utility modules that are
+in core python/numpy
 """
 
-# Define the version of neurite
+# set version
 __version__ = '0.2'
 
-# Third-party imports
+# check whether pystrum version is valid
 from packaging import version
 import pystrum
+minv = '0.2'
+curv = getattr(pystrum, '__version__', None)
+if curv is None or version.parse(curv) < version.parse(minv):
+    raise ImportError(f'neurite requires pystrum version {minv} or greater, '
+                      f'but found version {curv}')
 
-# Set the minimum allowable pystrum version
-_MIN_PYSTRUM_VERSION = "0.2"
-
-current_pystrum_version = getattr(pystrum, '__version__', None)
-
-# Determine if the installed version of pystrum is valid
-if (
-    current_pystrum_version is None or
-    version.parse(current_pystrum_version) <
-    version.parse(_MIN_PYSTRUM_VERSION)
-):
-    raise ImportError(
-        f'neurite requires pystrum version {_MIN_PYSTRUM_VERSION} or greater, but found version '
-        f'{current_pystrum_version}'
-    )
-
-# Immediate submodules
-from . import callbacks
-from . import data
-from . import generators
-from . import layers
-from . import losses
-from . import metrics
-from . import modelio
-from . import models
-from . import regularizers
-
-# Subpackages
-from . import utils
 from . import py
+from .py import utils
+from .py import plot
+from .py import dataproc
 
-# Import `py/` subpackage to namespace to enable non-explicit imports
-from .py import data, dataproc, plot
 
+# import backend-dependent submodules
+backend = py.utils.get_backend()
+if backend == 'pytorch':
+    # the pytorch backend can be enabled by setting the NEURITE_BACKEND
+    # environment var to "pytorch"
+    try:
+        import torch
+    except ImportError:
+        raise ImportError('Please install pytorch to use this neurite backend')
 
-__all__ = [
-    'callbacks',
-    'data',
-    'generators',
-    'layers',
-    'losses',
-    'metrics',
-    'modelio',
-    'models',
-    'regularizers',
-    'utils',
-    'py',
-    'plot',
-    'data',
-    'dataproc'
-]
+    from . import torch
+else:
+    # tensorflow is default backend
+    try:
+        import tensorflow
+    except ImportError:
+        raise ImportError('Please install tensorflow to use this neurite backend')
+
+    from . import tf
+    from .tf import *
